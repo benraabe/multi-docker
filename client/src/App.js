1eba65a70695e196a17f9fabc818fc4d964f6735
benjamin <benjaminraabe@unomaha.edu>
2020-01-15 18:55:59 +0100

    revert client changes

--- client/src/App.js
+++ client/src/App.js
@@ -10,19 +10,19 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
             <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+            <Link to="/otherPage">Other Page</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
+            <Route exact path="/otherPage" component={OtherPage} />
           </div>
         </div>
       </Router>
     );
   }
+
 }
 
 export default App;
